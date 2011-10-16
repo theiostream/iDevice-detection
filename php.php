0161d4262902523c872b2156c582a2fbcbab1dc3
James A. Matoe <daniel.calibeta@gmail.com>
2011-10-16 11:18:14 -0200

    Fix indentation

--- php.php
+++ php.php
@@ -3,11 +3,9 @@
 function getCurrentMachine() {
 	$ua = $_SERVER['HTTP_USER_AGENT'];
 	
-	if (preg_match('/iPad/', $ua)||preg_match('/iPhone/', 
-$ua)||preg_match('/iPod/', $ua)) return "iPhoneOS";
+	if (preg_match('/iPad/', $ua)||preg_match('/iPhone/', $ua)||preg_match('/iPod/', $ua)) return "iPhoneOS";
 	else if (preg_match('/Macintosh/', $ua)) return "MacOSX";
-	else if (preg_match('/MSIE/', $ua)||preg_match('/Windows/', 
-$ua)) return "Windows";
+	else if (preg_match('/MSIE/', $ua)||preg_match('/Windows/', $ua)) return "Windows";
 	else return "Unknown platform.";
 }
 
